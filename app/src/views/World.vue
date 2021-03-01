7abced6768a88fc5641e2a1bf060df6d93689028
predeezRav <prat0025@edu.ucl.dk>
2021-03-01 14:07:56 +0100

    Added country SASS

--- app/src/views/World.vue
+++ app/src/views/World.vue
@@ -2,16 +2,23 @@
   <div>
   <world-header></world-header>
   <search></search>
+  <country-container></country-container>
+
   </div>
 </template>
 
 <script>
 import Search from '@/components/Search'
 import WorldHeader from '@/components/worldHeader'
+import Country from '@/components/Country'
+
 
 export default {
   components: {
-    Search, WorldHeader
+    
+    Search, 
+    WorldHeader, 
+    Country
   }
 }
 </script>
