dd5c46e7093d24db0a5306660a94548c08b14a09
predeezRav <prat0025@edu.ucl.dk>
2021-03-01 15:13:47 +0100

    Added cards with flex!

--- app/src/components/CountryBox.vue
+++ app/src/components/CountryBox.vue
@@ -1,12 +1,59 @@
 <template>
-    <div class="country-container">
-        <div class="country">
-            <div class="country__flag"></div>
-            <div class="country__name">
-                <div class="country__info"></div>
+    <div class="country-container"> 
+        <div class="country">  
+            <div class="country__flag">Flag</div>
+            <div class="country__name">GErman
+                <div class="country__info">24654564564</div>
+            </div>
+        </div>
+        <div class="country">  
+            <div class="country__flag">Flag</div>
+            <div class="country__name">GErman
+                <div class="country__info">24654564564</div>
+            </div>
+        </div>
+
+        <div class="country">  
+            <div class="country__flag">Flag</div>
+            <div class="country__name">GErman
+                <div class="country__info">24654564564</div>
+            </div>
+        </div>
+
+        <div class="country">  
+            <div class="country__flag">Flag</div>
+            <div class="country__name">GErman
+                <div class="country__info">24654564564</div>
+            </div>
+        </div>
+         <div class="country">  
+            <div class="country__flag">Flag</div>
+            <div class="country__name">GErman
+                <div class="country__info">24654564564</div>
+            </div>
+        </div>
+         <div class="country">  
+            <div class="country__flag">Flag</div>
+            <div class="country__name">GErman
+                <div class="country__info">24654564564</div>
+            </div>
+        </div>
+
+         <div class="country">  
+            <div class="country__flag">Flag</div>
+            <div class="country__name">GErman
+                <div class="country__info">24654564564</div>
+            </div>
+        </div>
+
+         <div class="country">  
+            <div class="country__flag">Flag</div>
+            <div class="country__name">GErman
+                <div class="country__info">24654564564</div>
             </div>
         </div>
     </div>
+    
 </template>
 
 
