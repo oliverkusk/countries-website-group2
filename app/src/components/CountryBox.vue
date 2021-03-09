df5ce95d4a8a6a49efacdb04cdb2cc61d14ec831
predeezRav <prat0025@edu.ucl.dk>
2021-03-09 13:04:45 +0100

    Added data

--- app/src/components/CountryBox.vue
+++ app/src/components/CountryBox.vue
@@ -1,64 +1,87 @@
 <template>
-    <div class="country-container"> 
-        <div class="country">  
-            <div class="country__flag">Flag</div>
-            <div class="country__name">
-                <div class="country__info"></div>
-            </div>
-        </div>
-        <div class="country">  
-            <div class="country__flag">Flag</div>
-            <div class="country__name">GErman
-                <div class="country__info">24654564564</div>
-            </div>
-        </div>
-
-        <div class="country">  
-            <div class="country__flag">Flag</div>
-            <div class="country__name">GErman
-                <div class="country__info">24654564564</div>
-            </div>
-        </div>
-
-        <div class="country">  
-            <div class="country__flag">Flag</div>
-            <div class="country__name">GErman
-                <div class="country__info">24654564564</div>
-            </div>
-        </div>
-         <div class="country">  
-            <div class="country__flag">Flag</div>
-            <div class="country__name">GErman
-                <div class="country__info">24654564564</div>
-            </div>
-        </div>
-         <div class="country">  
-            <div class="country__flag">Flag</div>
-            <div class="country__name">GErman
-                <div class="country__info">24654564564</div>
-            </div>
-        </div>
-
-         <div class="country">  
-            <div class="country__flag">Flag</div>
-            <div class="country__name">GErman
-                <div class="country__info">24654564564</div>
-            </div>
-        </div>
-
-         <div class="country">  
-            <div class="country__flag">Flag</div>
-            <div class="country__name">GErman
-                <div class="country__info">24654564564</div>
-            </div>
-        </div>
+  <div class="country-container">
+    <div class="country">
+      <div class="country__flag"></div>
+      <div class="country__name">
+        <div class="country__info"></div>
+      </div>
     </div>
-    
+    <div class="country">
+      <div class="country__flag">Flag</div>
+      <div class="country__name">
+        GErman
+        <div class="country__info">24654564564</div>
+      </div>
+    </div>
+
+    <div class="country">
+      <div class="country__flag">Flag</div>
+      <div class="country__name">
+        GErman
+        <div class="country__info">24654564564</div>
+      </div>
+    </div>
+
+    <div class="country">
+      <div class="country__flag">Flag</div>
+      <div class="country__name">
+        GErman
+        <div class="country__info">24654564564</div>
+      </div>
+    </div>
+    <div class="country">
+      <div class="country__flag">Flag</div>
+      <div class="country__name">
+        GErman
+        <div class="country__info">24654564564</div>
+      </div>
+    </div>
+    <div class="country">
+      <div class="country__flag">Flag</div>
+      <div class="country__name">
+        GErman
+        <div class="country__info">24654564564</div>
+      </div>
+    </div>
+
+    <div class="country">
+      <div class="country__flag">Flag</div>
+      <div class="country__name">
+        GErman
+        <div class="country__info">24654564564</div>
+      </div>
+    </div>
+
+    <div class="country">
+      <div class="country__flag">Flag</div>
+      <div class="country__name">
+        GErman
+        <div class="country__info">24654564564</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      countries: [],
+    };
+  },
+  created() {
+    axios
+      .get(
+        "https://restcountries.eu/rest/v2/all?fields=name;capital;currencies"
+      )
+      .then((response) => (this.countries = response.data))
+      .catch((err) => console.log(err));
+  },
+};
 </script>
 <style lang="scss" >
-@import '@/assets/scss/country.scss';
+@import "@/assets/scss/country.scss";
 </style>
 
--- app/src/views/World.vue
+++ app/src/views/World.vue
@@ -1,23 +1,24 @@
 <template>
   <div>
-  <world-header></world-header>
-  <search-filter></search-filter>
-  <filter-region></filter-region>
-  
+    <world-header></world-header>
+    <search-filter></search-filter>
+    <filter-region></filter-region>
+    <country-container></country-container>
   </div>
-  <div><country-container></country-container></div>
 </template>
 
 <script>
-import SearchFilter from '@/components/SearchFilter'
-import FilterRegion from '@/components/FilterRegion'
-import WorldHeader from '@/components/worldHeader'
-import CountryContainer from '@/components/CountryBox'
-
+import SearchFilter from "@/components/SearchFilter";
+import FilterRegion from "@/components/FilterRegion";
+import WorldHeader from "@/components/worldHeader";
+import CountryContainer from "@/components/CountryBox";
 
 export default {
   components: {
-    SearchFilter, FilterRegion, WorldHeader, CountryContainer
-  }
-}
+    SearchFilter,
+    FilterRegion,
+    WorldHeader,
+    CountryContainer,
+  },
+};
 </script>
