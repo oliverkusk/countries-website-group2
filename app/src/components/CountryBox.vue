34a8ade3ac2ca19c76f06e1a3cfed2c539bc7c1c
predeezRav <prat0025@edu.ucl.dk>
2021-03-14 16:57:50 +0100

    updated countrybox

--- app/src/components/CountryBox.vue
+++ app/src/components/CountryBox.vue
@@ -12,7 +12,7 @@
     <ul>
       <li v-for="product in filterProductsByCategory" :key="product.earth">
         Product Name : {{ product.name }} - Price : {{ product.price }} ({{
-          product.category
+          country.region
         }})
       </li>
     </ul>
@@ -47,23 +47,10 @@ import axios from "axios";
 export default {
   data() {
     return {
-      category: "",
-      products: [
-        { name: "Keyboard", price: 44, category: "Accessories" },
-        { name: "Mouse", price: 20, category: "Accessories" },
-        { name: "Monitor", price: 399, category: "Accessories" },
-        { name: "Dell XPS", price: 599, category: "Laptop" },
-        { name: "MacBook Pro", price: 899, category: "Laptop" },
-        { name: "Pencil Box", price: 6, category: "Stationary" },
-        { name: "Pen", price: 2, category: "Stationary" },
-        { name: "USB Cable", price: 7, category: "Accessories" },
-        { name: "Eraser", price: 2, category: "Stationary" },
-        { name: "Highlighter", price: 5, category: "Stationary" },
-      ],
       computed: {
-        filterProductsByCategory: function () {
-          return this.products.filter(
-            (product) => !product.category.indexOf(this.category)
+        countries: function () {
+          return this.country.filter(
+            (country) => !country.countries.indexOf(this.country)
           );
         },
       },
@@ -109,3 +96,16 @@ data() {
       );
     },
   },
+
+
+   computed: {
+        countries: function () {
+          return this.country.filter(
+            (country) => !country.countries.indexOf(this.country)
+          );
+        },
+      },
+
+                  filterProductsByName: function() {
+                return this.products.filter(product => !product.name.indexOf(this.name))
+            },
